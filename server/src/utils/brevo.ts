461dcc1f50b307c50b0283ae28279b1c0b638eab
Shahzaib Afzal <theshahzaibafzal@gmail.com>
2024-09-08 15:47:33 +0500

    feat: Add function to send password reset email

--- server/src/utils/brevo.ts
+++ server/src/utils/brevo.ts
@@ -23,3 +23,29 @@ export const sendVerificationEmail = async (
     throw new Error("Failed to send email");
   }
 };
+
+export const resetPasswordEmail = async (
+  email: string,
+  token: string,
+  env: Bindings
+) => {
+  const resetPassword = `${env.RESET_PASSWORD_ROUTE}?token=${token}`;
+  const emailData = {
+    to: [{ email }],
+    templateId: env.RESET_TEMPLATE,
+    params: {
+      resetPassword,
+    },
+  };
+  const response = await fetch("https://api.brevo.com/v3/smtp/email", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "api-key": env.BREVO_API,
+    },
+    body: JSON.stringify(emailData),
+  });
+  if (!response.ok) {
+    throw new Error("Failed to send email");
+  }
+};
