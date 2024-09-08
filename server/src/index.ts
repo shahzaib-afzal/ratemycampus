04bb02cdabff88d75531107c4eadddd0819f5183
Shahzaib Afzal <theshahzaibafzal@gmail.com>
2024-09-08 21:56:22 +0500

    refactor: Update welcome message to version of Worker

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,7 @@ import { uniRoute } from "./routes/uni-route";
 const app = new Hono();
 
 app.get("/", async (c) => {
-  return c.text("Worker 1.0 (Test) is live");
+  return c.text("Worker 1.0 is live");
 });
 
 app.route("/api/v1/user/", userRoute);
