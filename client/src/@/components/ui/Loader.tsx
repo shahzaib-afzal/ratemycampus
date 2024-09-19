fdde1c7c0174769f59a34db7ae5b2d0380d34f58
Shahzaib Afzal <theshahzaibafzal@gmail.com>
2024-09-19 17:02:52 +0500

    refactor: Update Loader component to use custom colors for the loading animation

--- client/src/@/components/ui/Loader.tsx
+++ client/src/@/components/ui/Loader.tsx
@@ -1,6 +1,14 @@
 import { motion } from "framer-motion";
 
-export const Loader = ({ size = 24 }: { size?: number }) => (
+export const Loader = ({
+  size = 24,
+  color1 = "#007BFF",
+  color2 = "#FF33CC",
+}: {
+  size?: number;
+  color1?: string;
+  color2?: string;
+}) => (
   <motion.div
     className={`relative mx-auto`}
     style={{ width: `${size}px`, height: `${size}px` }}
@@ -8,12 +16,14 @@ export const Loader = ({ size = 24 }: { size?: number }) => (
     transition={{ duration: 2, repeat: Infinity, ease: "linear" }}
   >
     <motion.div
-      className="absolute inset-0 rounded-full border-t-4 border-blue-500"
+      className="absolute inset-0 rounded-full border-t-4"
+      style={{ borderColor: color1 }}
       animate={{ scale: [1, 1.2, 1], opacity: [1, 0.8, 1] }}
       transition={{ duration: 1.5, repeat: Infinity, ease: "easeInOut" }}
     />
     <motion.div
-      className="absolute inset-0 rounded-full border-r-4 border-purple-500"
+      className="absolute inset-0 rounded-full border-r-4"
+      style={{ borderColor: color2 }}
       animate={{ scale: [1.2, 1, 1.2], opacity: [0.8, 1, 0.8] }}
       transition={{ duration: 1.5, repeat: Infinity, ease: "easeInOut" }}
     />
