5ed0d0b4d415ad205556bd218c33a54cbf35274e
Shahzaib Afzal <theshahzaibafzal@gmail.com>
2024-09-20 23:31:06 +0500

    refactor: Add CORS middleware for API routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,19 @@
 import { Hono } from "hono";
 import { userRoute } from "./routes/user-route";
 import { uniRoute } from "./routes/uni-route";
+import { cors } from "hono/cors";
 
 const app = new Hono();
 
+app.use(
+  "/api/*",
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
 app.get("/", async (c) => {
-  return c.text("Worker 1.0 is live");
+  return c.text("Worker 1.1 is live");
 });
 
 app.route("/api/v1/user/", userRoute);
