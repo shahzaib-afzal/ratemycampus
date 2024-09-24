a688602d5e24108f0335283144fcc5dd90fc486d
Shahzaib Afzal <theshahzaibafzal@gmail.com>
2024-09-24 17:04:41 +0500

    refactor: Refactor Result component to improve readability and flexibility
    - Extracted ResultProps interface to provide more flexibility in customizing the component
    - Renamed success and error headlines for better clarity
    - Moved success and error messages to separate props for easier customization

--- client/src/@/components/ui/Result.tsx
+++ client/src/@/components/ui/Result.tsx
@@ -1,14 +1,28 @@
 import { motion } from "framer-motion";
 import { CheckCircle, XCircle } from "lucide-react";
 
-export const Result = ({ success }: { success: boolean }) => (
+interface ResultProps {
+  success: boolean;
+  headlineSuccess?: string;
+  headlineError?: string;
+  successMessage?: string;
+  failureMessage?: string;
+}
+
+export const Result = ({
+  success,
+  headlineSuccess = "Email Verified!",
+  headlineError = "Verification Failed",
+  successMessage = "Your email has been successfully verified. Welcome to RateMyCampus!",
+  failureMessage = "We encountered an issue verifying your email. Please try again or contact support.",
+}: ResultProps) => (
   <motion.div
     initial={{ opacity: 0, y: 20 }}
     animate={{ opacity: 1, y: 0 }}
     transition={{ duration: 0.5 }}
     className={`flex flex-col items-center rounded-lg p-8 ${
       success ? "bg-green-500 bg-opacity-20" : "bg-red-500 bg-opacity-20"
-    }`}
+    } text-white`}
   >
     {success ? (
       <CheckCircle className="mb-4 h-16 w-16 text-green-500" />
@@ -16,12 +30,10 @@ export const Result = ({ success }: { success: boolean }) => (
       <XCircle className="mb-4 h-16 w-16 text-red-500" />
     )}
     <h2 className="mb-2 text-center text-2xl font-bold">
-      {success ? "Email Verified!" : "Verification Failed"}
+      {success ? headlineSuccess : headlineError}
     </h2>
     <p className="text-center text-sm md:text-base">
-      {success
-        ? "Your email has been successfully verified. Welcome to RateMyCampus!"
-        : "We encountered an issue verifying your email. Please try again or contact support."}
+      {success ? successMessage : failureMessage}
     </p>
   </motion.div>
 );
