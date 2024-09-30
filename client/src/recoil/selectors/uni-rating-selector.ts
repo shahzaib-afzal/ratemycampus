1dd623af2f4f2e70526303fb0d2fe70b3c1a1777
Shahzaib Afzal <theshahzaibafzal@gmail.com>
2024-09-30 21:34:46 +0500

    Refactor ratingSelector to include triggerAtom and optimize token retrieval

--- client/src/recoil/selectors/uni-rating-selector.ts
+++ client/src/recoil/selectors/uni-rating-selector.ts
@@ -1,15 +1,20 @@
 import { selector } from "recoil";
 import axios from "axios";
 import { Rating } from "@/types";
+import { triggerAtom } from "../atoms/trigger-atom";
 
 export const ratingSelector = selector({
   key: "ratingSelector",
-  get: async () => {
+  get: async ({ get }) => {
     try {
+      get(triggerAtom);
+      const token = localStorage.getItem("Authorization");
+      if (!token) throw new Error();
+
       const backendUrl = import.meta.env.VITE_BACKEND_URL_PROD;
       const response = await axios.get(`${backendUrl}/uni/get-rating`, {
         headers: {
-          Authorization: `Bearer ${localStorage.getItem("Authorization")}`,
+          Authorization: `Bearer ${token}`,
         },
       });
       return response.data.ratings as Rating[];
